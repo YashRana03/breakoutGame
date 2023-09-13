e19f73f5a5417094090d9f4ccfe030962089c666
Yash rana <97224327+YashRana03@users.noreply.github.com>
2023-09-13 13:28:16 +0100

    Add game grid

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const startGameH3El = document.querySelector("#start-game h3")
 const startGameEl = document.getElementById("start-game")
+const gameEl = document.getElementById("game")
+const colours = ["pink", "blue", "green", "orange"]
 let value = 0.1
+let count = -1
+
 
 startGameEl.addEventListener("click", () => {
     startGameEl.style.animation = "slideleft 1s ease-in"
@@ -19,3 +23,23 @@ setInterval(() => {
         value = 0.1
     }
 }, 1000)
+
+
+function renderBlocks() {
+    
+    let something = null
+    
+    for(let i = 0; i<20; i++) {
+        if(i % 5 == 0) {
+            count++
+        }
+        something = document.createElement("div")
+        something.style.backgroundColor = colours[count]
+        something.classList.add("block")
+        gameEl.appendChild(something)
+        
+    }
+    
+}
+
+renderBlocks()
