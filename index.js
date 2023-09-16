a9613b12f1db07889953f7e206db79d6dacf973d
Yash rana <97224327+YashRana03@users.noreply.github.com>
2023-09-16 18:14:48 +0100

    Make game replayable

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const startGameH3El = document.querySelector("#start-game h3")
 const startGameEl = document.getElementById("start-game")
 const gameEl = document.getElementById("game")
 const livesEl = document.querySelector("#lives span")
-console.log(livesEl)
+const scoreEl = document.getElementById("score")
+const messageEl = document.createElement("h3")
+const containerEl = document.getElementById("container")
+
+
 const heart = "❤"
 const colours = ["pink", "blue", "green", "orange"]
 let value = 0.1
 const blockWidth = 150
 const blockHeight = 30
 let paddleWidth = 150
-//let count = -1
-let adderX = 2
-let adderY = 2
+let adderX = 4
+let adderY = 4
 let timerId = null
 let lives = 3
+let score = 0
+let scale = 1.1
 
 
 startGameEl.addEventListener("click", () => {
@@ -33,6 +38,7 @@ setInterval(() => {
     else {
         value = 0.1
     }
+    
 }, 1000)
 
 
@@ -46,7 +52,7 @@ class Block {
 }
 
 const paddlePosition = [375, 450]
-let ballPosition = [250, 250]
+let ballPosition = [250, 200]
 let allBlocks = [
     new Block(55, 30),
     new Block(215, 30), 
@@ -69,15 +75,18 @@ const allBlocksCopy = [...allBlocks]
 
 
 
-let block = null
-for(let i = 0; i<allBlocks.length; i++) {
-    block = document.createElement("div")
-    block.classList.add("block")
-    block.style.left = allBlocks[i].bottomLeft[0]  + "px"
-    block.style.top = allBlocks[i].bottomLeft[1] + "px"
-    gameEl.appendChild(block)
+function displayBlocks() {
+    let block = null
+    for(let i = 0; i<allBlocks.length; i++) {
+        block = document.createElement("div")
+        block.classList.add("block")
+        block.style.left = allBlocks[i].bottomLeft[0]  + "px"
+        block.style.top = allBlocks[i].bottomLeft[1] + "px"
+        gameEl.appendChild(block)
+    }
 }
 
+displayBlocks()
 
 const paddle = document.createElement("div")
 paddle.classList.add("paddle")
@@ -89,13 +98,13 @@ function movePaddle(e) {
     switch(e.key) {
         case "ArrowLeft":
             if(paddlePosition[0] > 15)
-            paddlePosition[0] -= 20
+            paddlePosition[0] -= 30
             paddle.style.left = paddlePosition[0] + "px"
             break
 
         case "ArrowRight":
             if(paddlePosition[0] < 725)
-            paddlePosition[0] += 20
+            paddlePosition[0] += 30
             paddle.style.left = paddlePosition[0] + "px"
             break
     }
@@ -131,7 +140,7 @@ function checkCollision() {
     if( (ballPosition[1] > 424 && ballPosition[1] <= 428) && (ballPosition[0] >= paddlePosition[0] -10 && ballPosition[0] <= paddlePosition[0] + paddleWidth + 10)) {
 
         if(ballPosition[0] < paddlePosition[0] + 50) {
-            adderX = -2
+            adderX = -4
             adderY = -adderY
             }
 
@@ -140,7 +149,7 @@ function checkCollision() {
             adderY = -adderY
         }
         else {
-            adderX = 2
+            adderX = 4
             adderY = -adderY
         }
         
@@ -148,30 +157,35 @@ function checkCollision() {
 
     if(ballPosition[1] >= 495 ) {
         
-        console.log("You lost")
+        
         ball.style.opacity = 0
         clearInterval(timerId)
         lives--
         if(lives == 0) {
+            let els = document.querySelectorAll("#game .block")
+            for(el of els) {
+                el.remove()
+            }
+            setLives()
             console.log("Game finished")
+            messageEl.textContent = "You Lose"
+            scoreEl.style.opacity = 0
+            createDialogueBox() 
         }
         else {
             setTimeout(()=> {
-            
-            
-                console.log(lives)
-                console.log(livesEl)
-                ballPosition = [250, 250]
+
+                ballPosition = [250, 150]
                 setPosition(ball, ballPosition)
                 adderX = 2
                 adderY = 2
                 ball.style.opacity = 1
                 startGame()
+
             }, 1000)
         }
         
     }
-    
     
     let blocksEl = document.querySelectorAll("#game .block")
     for(let i = 0; i<allBlocks.length; i++) {
@@ -181,6 +195,8 @@ function checkCollision() {
             blocksEl[i].remove()
             allBlocks.splice(i, 1) 
             adderY = -adderY
+            score += 10
+            scoreEl.textContent = `Score: ${score}`
 
         }
     }
@@ -200,7 +216,65 @@ function setLives() {
 
 }
 
+
+function createDialogueBox() {
+    
+    const dialogueEl = document.createElement("div")
+    const h1El = document.createElement("h1")
+    const h3El = document.createElement("h3")
+    const buttonEl = document.createElement("button")
+
+    h1El.classList.add("game-over")
+    h1El.textContent = "Game Over"
+    dialogueEl.appendChild(h1El)
+
+    h3El.classList.add("score")
+    h3El.textContent = `Score: ${score}`
+    dialogueEl.appendChild(h3El)
+
+    buttonEl.classList.add("replay-btn")
+    buttonEl.textContent = "Play again"
+    buttonEl.addEventListener("mouseover", () => {
+        buttonEl.style.backgroundColor = "#85458A"
+    })
+
+    buttonEl.addEventListener("mouseout", () => {
+        buttonEl.style.backgroundColor = "#DE3456"
+    })
+
+    buttonEl.addEventListener("click", () => {
+        allBlocks = [...allBlocksCopy]
+        score = 0
+        lives = 3
+        displayBlocks()
+        ballPosition = [250, 150]
+        ball.style.opacity = 1
+        scoreEl.style.opacity = 1
+        startGame()
+        dialogueEl.remove()
+    })
+    
+    dialogueEl.appendChild(buttonEl)
+
+    setInterval(() => {
+        buttonEl.style.scale = scale
+        if(scale == 1.1) {
+            scale = 1
+        }
+        else {
+            scale = 1.1
+        }
+    }, 900)
+
+    dialogueEl.classList.add("dialogue")
+    containerEl.appendChild(dialogueEl)
+    
+
+
+}
 function startGame() {
+    // displayBlock()
+    
     setLives()
     timerId = setInterval(moveBall, 10)
 }
